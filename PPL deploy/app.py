d61de2f5bce6d02224df43922688f8142708d60b
Yusuf Ginanjar <119514056+YusufGinanjar7@users.noreply.github.com>
2025-06-24 17:50:42 +0700

    Add chatbot backend API files

--- PPL deploy/app.py
+++ PPL deploy/app.py
@@ -1,21 +1,16 @@
 from fastapi import FastAPI, Request
-from transformers import AutoModelForCausalLM, AutoTokenizer
-import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 app = FastAPI()
 
-model_path = "D:\Kuliah\Kuliah\kuliah semester 6\PPL deploy\model\DialoGPT-medium"  # path ke file yg kamu download
-
-tokenizer = AutoTokenizer.from_pretrained(model_path)
-model = AutoModelForCausalLM.from_pretrained(model_path)
+tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
+model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
 
 @app.post("/chat")
 async def chat(request: Request):
     data = await request.json()
     user_input = data.get("message", "")
-
     inputs = tokenizer.encode(user_input + tokenizer.eos_token, return_tensors="pt")
-    response = model.generate(inputs, max_length=100, pad_token_id=tokenizer.eos_token_id)
-    reply = tokenizer.decode(response[:, inputs.shape[-1]:][0], skip_special_tokens=True)
-
-    return {"response": reply}
+    outputs = model.generate(inputs, max_length=100, pad_token_id=tokenizer.eos_token_id)
+    response = tokenizer.decode(outputs[:, inputs.shape[-1]:][0], skip_special_tokens=True)
+    return {"response": response}
